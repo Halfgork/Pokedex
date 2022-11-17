3f6aa633e551398c9147e3e65399136ef4dbeb20
Halfgork <gorkemsakalak@gmail.com>
2022-11-17 12:57:03 +0300

    updated

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import PokeGame from './PokeGame';
+import './PokeCard.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Pokemon Card Game</h1>
+      <PokeGame />
+    </div>
+  );
+}
+
+export default App;
